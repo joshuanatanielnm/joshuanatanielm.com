5e5841ab177e22be717a7f3b378ac2b98c476f23
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2024-03-15 03:54:34 +0700

    feat: create links variable

---
+++ app/(default)/links.ts
@@ -0,0 +1,31 @@
+import {
+  CalendarIcon,
+  EnvelopeClosedIcon,
+  FileIcon,
+  GitHubLogoIcon,
+  LinkedInLogoIcon,
+  TwitterLogoIcon,
+} from "@radix-ui/react-icons";
+import { customMetadata } from "@/site.config";
+
+export const links = [
+  { href: customMetadata.resumeUrl, Icon: FileIcon, label: "Resume" },
+  { href: customMetadata.twitterUrl, Icon: TwitterLogoIcon, label: "Twitter" },
+  { href: customMetadata.githubUrl, Icon: GitHubLogoIcon, label: "Github" },
+  {
+    href: customMetadata.linkedInUrl,
+    Icon: LinkedInLogoIcon,
+    label: "Linkedin",
+  },
+  {
+    href: customMetadata.emailUrl,
+    Icon: EnvelopeClosedIcon,
+    label: "Email",
+  },
+];
+
+export const homeNavLinks = [
+  { href: "#about", label: "About" },
+  { href: "#experiences", label: "Experiences" },
+  { href: "#projects", label: "Projects" },
+];
