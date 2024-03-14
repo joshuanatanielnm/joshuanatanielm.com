21c72d65bc32f71ad945b6124a2bd2a04df42b92
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2024-03-15 03:51:32 +0700

    feat: create site config

---
+++ site.config.ts
@@ -0,0 +1,19 @@
+export const defaultMetadata = {
+  title: "Joshua Nathaniel Manuputty",
+  description:
+    "Software engineer based in Indonesia 🇮🇩, specializing in Frontend Development using React-based technologies.",
+  email: "joshuanmanuputty@gmail.com",
+  github: {
+    username: "joshuanatanielnm",
+    repository: "joshuanatanielm.com-v3",
+  },
+};
+
+export const customMetadata = {
+  githubUrl: "https://github.com/joshuanatanielnm",
+  twitterUrl: "https://twitter.com/joshuanatanielm",
+  emailUrl: `mailto:${defaultMetadata.email}`,
+  resumeUrl: "https://resume.io/r/fyzF2RrFm",
+  linkedInUrl: "https://www.linkedin.com/in/joshuanathanielm",
+  calUrl: "https://cal.com/joshuanatanielm",
+};
