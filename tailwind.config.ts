60f95a0b2bbea6eed36c062beed3a1f76a0249b4
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2024-03-31 03:35:48 +0700

    feat: update duration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -81,7 +81,7 @@ const config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        slidein: "slidein 1s ease 300ms",
+        slidein: "slidein ease 700ms",
       },
     },
   },
