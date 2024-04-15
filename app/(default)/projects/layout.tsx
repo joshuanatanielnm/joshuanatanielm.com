6d371318a690597973a40a1d2fc00f58fe7e8cbd
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2024-04-15 23:38:40 +0700

    feat: update button location for mobile

--- app/(default)/projects/layout.tsx
+++ app/(default)/projects/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: ReactNode }) {
     <div>
       {children}
       <Link
-        className="fixed bottom-0 mb-8 border p-2 rounded-lg border-solid border-orange-300 bg-orange-50 text-orange-500 hover:text-orange-700 hover:border-orange-400 hover:bg-orange-100"
+        className="fixed bottom-0 right-4 lg:right-auto mb-8 border p-2 rounded-lg border-solid border-orange-300 bg-orange-50 text-orange-500 hover:text-orange-700 hover:border-orange-400 hover:bg-orange-100"
         href="/"
       >{`<- Go Back`}</Link>
       <div className="flex py-14">
