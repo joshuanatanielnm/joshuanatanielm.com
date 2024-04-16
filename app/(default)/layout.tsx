36f39a9dc3bda7c835186e7adce49e62e9be74c3
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2024-04-17 00:30:38 +0700

    core: install and setup react query

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,5 +1,14 @@
+"use client";
+
 import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <main>{children}</main>;
+  return (
+    <main>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </main>
+  );
 }
